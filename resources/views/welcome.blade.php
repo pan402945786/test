d384e74aba07bfd963a17ad8e25a9163e86debf6
Highland Pan <402945786@qq.com>
2018-05-06 04:53:08 +0800

    文字修改

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel,hello world!!!
+                    Laravel,hello 西居宅!!!
                 </div>
 
                 <div class="links">
