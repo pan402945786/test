1d7cbc72d952442b0ce447d2c9ffc1479d16d143
Highland Pan <402945786@qq.com>
2018-05-19 17:45:10 +0800

    word changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel,hello world!!!</title>
-
+        <title>欢迎来到西居宅</title>
+        <link rel="shortcut icon" href="/bitbug_favicon.ico" />
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel,hello 西居宅!!!
+                  Hello 西居宅
                 </div>
 
                 <div class="links">
